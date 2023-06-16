575d3ab11c414eddd62783302c4e594e86854912
Wendy Kuo <weimmod@gmail.com>
2023-06-16 12:19:09 -0500

    refactor the issue about sqme user has towo booking on one spot

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -20,155 +20,186 @@ router.get('/current', requireAuth, async (req, res, next) => {
       {
         model: Spot,
         attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
-        include: [
-          {
-            model: SpotImage,
-            attributes: ['url', 'preview']
-          }
-        ]
+        // include: [
+        //   {
+        //     model: SpotImage,
+        //     attributes: ['url', 'preview']
+        //   }
+        // ]
       }
     ]
   });
   // console.log(bookings);
   let bookingList = [];
+  let spotImageList = []   //del
   bookings.forEach(booking => {
     bookingList.push(booking.toJSON());
+    spotImageList.push(booking.spotId)  //del
   });
-  bookingList.forEach(booking => {
-    booking.Spot.SpotImages.forEach(image => {
-      if (image.preview === true) {
-        booking.Spot.previewImage = image.url;
-      }
-    })
-    if (!booking.Spot.previewImage) {
-      booking.Spot.previewImage = null;
-    }
-    delete booking.Spot.SpotImages;
-  })
-  res.json({ Bookings: bookingList })
-})
+
+  // bookingList.forEach(booking => {
+  //   booking.Spot.SpotImages.forEach(image => {
+  //     if (image.preview === true) {
+  //       booking.Spot.previewImage = image.url;
+  //     }
+  //   })
+  //   if (!booking.Spot.previewImage) {
+  //     booking.Spot.previewImage = null;
+  //   }
+  //   delete booking.Spot.SpotImages;
+  // })
+  // res.json({ Bookings: bookingList })
 
 
-
-//Edit a Booking
-//PUT /bookings/:id
-//Body validation errors come from db level(model file)
-router.put('/:id', requireAuth, async (req, res, next) => {
-  const { startDate, endDate } = req.body;
-  const { user } = req;
-
-  let oneBooking = await Booking.findOne({
+  const spotImages = await SpotImage.findAll({
     where: {
-      id: req.params.id
-    }
-  })
-
-  if (!oneBooking) {
-    res.statusCode = 404
-    return res.json({ 'message': "Booking couldn't be found" })
-  }
-  const oneBookingPOJO = oneBooking.toJSON();
-  // console.log(oneBookingPOJO);
-  //Redundant work, already define in model file
-  //Body validation errors come from db level(model file)
-  if (oneBookingPOJO.endDate < oneBookingPOJO.startDate) {
-    res.statusCode = 400;
-    return res.json("endDate cannot come before startDate")
-  }
-
-  let currentDate = new Date();
-
-  let newStartDate = new Date(oneBookingPOJO.startDate);
-
-  if (newStartDate < currentDate) {
-    res.statusCode = 403;
-    return res.json({ 'message': "Past bookings can't be modified" })
-  }
-
-  if (oneBooking.userId != user.id) {
-    res.statusCode = 403;
-    return res.json({ 'message': "Forbidden" })
-  }
-//test conflictBooking
-  const conflictBooking = await Booking.findOne({
-    where: {
-      [Op.or]: [
-        { startDate: { [Op.between]: [startDate, endDate] } },
-        { endDate: { [Op.between]: [startDate, endDate] } }
-      ],
-      spotId: oneBooking.spotId
-    }
-  })
-  // console.log(conflictBooking)
-  if (conflictBooking) {
-    res.statusCode = 403;
-    return res.json({
-      message: "Sorry, this spot is already booked for the specified dates",
-      errors: {
-        startDate: "Start date conflicts with an existing booking",
-        endDate: "End date conflicts with an existing booking"
-      }
-    })
-  }
-
-
-
-
-  let setObj = {}
-  if (startDate) {
-    setObj.startDate = startDate;
-  }
-  if (endDate) {
-    setObj.endDate = endDate;
-  }
-  oneBooking.set(setObj);
-  await oneBooking.save();
-  res.json(oneBooking);
-
-})
-
-///delete bookings/:id
-//Delete a Booking
-router.delete('/:id', requireAuth, async (req, res, next) => {
-  const { user } = req;
-  const oneBooking = await Booking.findOne({
-    where: {
-      id: req.params.id
+      spotId: { [Op.in]: spotImageList }
     },
-    include: [
-      {
-        model: Spot,
-        attributes: ['ownerId']
-      }
-    ]
+    attributes: ['url', 'preview']
+  })
+
+  let imagesList = [];
+  spotImages.forEach(image => {
+    imagesList.push(image.toJSON());
   });
-  if (!oneBooking) {
-    res.statusCode = 404
-    return res.json({ 'message': "Booking couldn't be found" })
-  }
-  let newoneBookingPOJO = oneBooking.toJSON();
-  console.log(newoneBookingPOJO)
+  // console.log(imagesList)
 
-  if (newoneBookingPOJO.Spot.ownerId !== user.id && newoneBookingPOJO.userId !== user.id) {
-    console.log(newoneBookingPOJO.Spot.ownerId, newoneBookingPOJO.userId)
-
-    res.statusCode = 403;
-    return res.json({ 'message': "Forbidden" })
-  }
-  let currentDate = new Date();
-  let startDate = new Date(newoneBookingPOJO.startDate);
-  let endDate = new Date(newoneBookingPOJO.endDate);
-  if (startDate < currentDate && endDate > currentDate) {
-    res.statusCode = 403;
-    return res.json({
-      message: "Bookings that have been started can't be deleted."
-    })
-  }
-
-  await oneBooking.destroy();
-  res.json({ "message": "Successfully deleted" })
-
-
+  bookingList.forEach(booking => {
+    if (!imagesList) {
+      booking.Spot.previewImage = null;
+    } else if (imagesList) {
+      imagesList.forEach(image => {
+        imagesList.forEach(image => {
+          if (image.preview == true) {
+            booking.Spot.previewImage = image.url;
+          }
+        })
+      })
+    }
+    res.json({ Bookings: bookingList })
+  })
 })
 
-module.exports = router;
+
+  //Edit a Booking
+  //PUT /bookings/:id
+  //Body validation errors come from db level(model file)
+  router.put('/:id', requireAuth, async (req, res, next) => {
+    const { startDate, endDate } = req.body;
+    const { user } = req;
+
+    let oneBooking = await Booking.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if (!oneBooking) {
+      res.statusCode = 404
+      return res.json({ 'message': "Booking couldn't be found" })
+    }
+    const oneBookingPOJO = oneBooking.toJSON();
+    // console.log(oneBookingPOJO);
+    //Redundant work, already define in model file
+    //Body validation errors come from db level(model file)
+    if (oneBookingPOJO.endDate < oneBookingPOJO.startDate) {
+      res.statusCode = 400;
+      return res.json("endDate cannot come before startDate")
+    }
+
+    let currentDate = new Date();
+
+    let newStartDate = new Date(oneBookingPOJO.startDate);
+
+    if (newStartDate < currentDate) {
+      res.statusCode = 403;
+      return res.json({ 'message': "Past bookings can't be modified" })
+    }
+
+    if (oneBooking.userId != user.id) {
+      res.statusCode = 403;
+      return res.json({ 'message': "Forbidden" })
+    }
+    //test conflictBooking
+    const conflictBooking = await Booking.findOne({
+      where: {
+        [Op.or]: [
+          { startDate: { [Op.between]: [startDate, endDate] } },
+          { endDate: { [Op.between]: [startDate, endDate] } }
+        ],
+        spotId: oneBooking.spotId
+      }
+    })
+    // console.log(conflictBooking)
+    if (conflictBooking) {
+      res.statusCode = 403;
+      return res.json({
+        message: "Sorry, this spot is already booked for the specified dates",
+        errors: {
+          startDate: "Start date conflicts with an existing booking",
+          endDate: "End date conflicts with an existing booking"
+        }
+      })
+    }
+
+
+
+
+    let setObj = {}
+    if (startDate) {
+      setObj.startDate = startDate;
+    }
+    if (endDate) {
+      setObj.endDate = endDate;
+    }
+    oneBooking.set(setObj);
+    await oneBooking.save();
+    res.json(oneBooking);
+
+  })
+
+  ///delete bookings/:id
+  //Delete a Booking
+  router.delete('/:id', requireAuth, async (req, res, next) => {
+    const { user } = req;
+    const oneBooking = await Booking.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Spot,
+          attributes: ['ownerId']
+        }
+      ]
+    });
+    if (!oneBooking) {
+      res.statusCode = 404
+      return res.json({ 'message': "Booking couldn't be found" })
+    }
+    let newoneBookingPOJO = oneBooking.toJSON();
+    console.log(newoneBookingPOJO)
+
+    if (newoneBookingPOJO.Spot.ownerId !== user.id && newoneBookingPOJO.userId !== user.id) {
+      console.log(newoneBookingPOJO.Spot.ownerId, newoneBookingPOJO.userId)
+
+      res.statusCode = 403;
+      return res.json({ 'message': "Forbidden" })
+    }
+    let currentDate = new Date();
+    let startDate = new Date(newoneBookingPOJO.startDate);
+    let endDate = new Date(newoneBookingPOJO.endDate);
+    if (startDate < currentDate && endDate > currentDate) {
+      res.statusCode = 403;
+      return res.json({
+        message: "Bookings that have been started can't be deleted."
+      })
+    }
+
+    await oneBooking.destroy();
+    res.json({ "message": "Successfully deleted" })
+
+
+  })
+
+  module.exports = router;
