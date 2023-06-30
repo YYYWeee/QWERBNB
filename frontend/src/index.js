87c8d29634d65b5df76fef37e3997c1c860de825
Wendy Kuo <weimmod@gmail.com>
2023-06-30 14:36:46 -0500

    loginform page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF,csrfFetch } from './store/csrf';
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
+
+// if (process.env.NODE_ENV !== "production") {
+//   window.store = store;
+// }
 
 function Root() {
   return (
